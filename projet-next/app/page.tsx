786d5b7a1b7f5b8c02a576822970a5c5463e4fc9
YacineBoukliff <yacine69800@live.fr>
2024-12-18 12:13:00 +0100

    TestCommit Vscode

--- projet-next/app/page.tsx
+++ projet-next/app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
   return (
    <div>
     <DiAndroid /> 
-    
+    <Avatar>
+  <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarFallback>CN</AvatarFallback>
+</Avatar>
     
    </div>
   );
