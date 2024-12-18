f498ff2db676d6ef700acbd9200240d334ce6157
YacineBoukliff <yacine69800@live.fr>
2024-12-18 12:14:20 +0100

    Envoi commit github desktop

--- projet-next/app/page.tsx
+++ projet-next/app/page.tsx
@@ -6,10 +6,20 @@ export default function Home() {
   return (
    <div>
     <DiAndroid /> 
+
+
     <Avatar>
   <AvatarImage src="https://github.com/shadcn.png" />
   <AvatarFallback>CN</AvatarFallback>
 </Avatar>
+
+<div>
+
+<Avatar>
+  <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarFallback>CN</AvatarFallback>
+</Avatar>
+</div>
     
    </div>
   );
