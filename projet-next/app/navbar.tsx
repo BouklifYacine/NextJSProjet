d631d0f13c61c158dd0ac096ae0ce835357479f7
YacineBoukliff <yacine69800@live.fr>
2024-12-18 12:50:18 +0100

    Affichage dynamique de la navbar avec le .map

--- projet-next/app/navbar.tsx
+++ projet-next/app/navbar.tsx
@@ -4,6 +4,15 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 
 const Navbar = () => {
+
+  // ici je gère les liens dans la navbar
+
+const liens = [
+  {label : "Accueil" , lien : "/"},
+  {label : "Crud" , lien : "/crud"}
+
+]
+
   return (
     <nav className="w-3/4 mx-auto bg-gray-300 rounded-xl mt-6 px-4 py-3 flex items-center justify-between shadow-sm">
   
@@ -13,23 +22,16 @@ const Navbar = () => {
 
  
     <div className="flex items-center gap-6">
-     
-      <Link 
-        href="/" 
-        className="text-blue-500 hover:text-blue-700 transition-colors text-lg"
-      >
-        Accueil
-      </Link>
 
-      <Link 
-        href="/crud" 
-        className="text-blue-500 hover:text-blue-700 transition-colors text-lg"
-      >
-        Crud
-      </Link>
+      {liens.map(lien =>  <Link key={lien.label} href={lien.lien} className="text-black hover:text-blue-700 transition-colors text-lg">
+       {lien.label}
+      </Link> )}
+   
     </div>
 
  
+{/* On va gérer l'image de l'avatar quand on sera a la partie de l'authentification */}
+
     <Avatar className="h-8 w-8">
       <AvatarImage src="https://github.com/shadcn.png" alt="User" />
       <AvatarFallback>CN</AvatarFallback>
