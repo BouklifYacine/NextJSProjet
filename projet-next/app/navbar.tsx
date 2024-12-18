6b45f1c5a508fc88d3d49e211d81726fd6a1c14c
YacineBoukliff <yacine69800@live.fr>
2024-12-18 13:15:39 +0100

    Ajout dynamique des liens et typage avec typescript pour corriger le param de la fn qui avait un type any

--- projet-next/app/navbar.tsx
+++ projet-next/app/navbar.tsx
@@ -1,42 +1,61 @@
+"use client"
+
 import React from 'react'
-import { DiAndroid } from "react-icons/di";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Link from "next/link";
+import { DiAndroid } from "react-icons/di"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Link from "next/link"
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames'
+
+type Lien = {
+  label: string
+  lien: string
+}
 
 const Navbar = () => {
+  const liens: Lien[] = [
+    {label: "Accueil", lien: "/"},
+    {label: "Crud", lien: "/crud"}
+  ]
 
-  // ici je gère les liens dans la navbar
+  const Direction = usePathname()
+  console.log(Direction)
 
-const liens = [
-  {label : "Accueil" , lien : "/"},
-  {label : "Crud" , lien : "/crud"}
 
-]
+  const getLinkClasses = (lien: string): string => {
+    return classNames(
+      "transition-colors",
+      {
+        "text-blue-600": lien === Direction,
+        "text-blue-300": lien !== Direction,
+        "hover:text-blue-700 font-bold": true,
+      }
+    )
+  }
 
   return (
     <nav className="w-3/4 mx-auto bg-gray-300 rounded-xl mt-6 px-4 py-3 flex items-center justify-between shadow-sm">
-  
-    <div className="flex items-center">
-      <DiAndroid className="text-2xl text-blue-600" />
-    </div>
+      <div className="flex items-center">
+        <DiAndroid className="text-2xl text-blue-600" />
+      </div>
 
- 
-    <div className="flex items-center gap-6">
+      <div className="flex items-center gap-6">
+        {liens.map(lien => (
+          <Link 
+            key={lien.label} 
+            href={lien.lien}
+            className={getLinkClasses(lien.lien)}
+          >
+            {lien.label}
+          </Link>
+        ))}
+      </div>
 
-      {liens.map(lien =>  <Link key={lien.label} href={lien.lien} className="text-black hover:text-blue-700 transition-colors text-lg">
-       {lien.label}
-      </Link> )}
-   
-    </div>
-
- 
-{/* On va gérer l'image de l'avatar quand on sera a la partie de l'authentification */}
-
-    <Avatar className="h-8 w-8">
-      <AvatarImage src="https://github.com/shadcn.png" alt="User" />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
-  </nav>
+      <Avatar className="h-8 w-8">
+        <AvatarImage src="https://github.com/shadcn.png" alt="User" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </nav>
   )
 }
 
