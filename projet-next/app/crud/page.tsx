e2842918eee0b86793fbbe60101285c6c8c5ab17
YacineBoukliff <yacine69800@live.fr>
2024-12-18 17:34:36 +0100

    Ajout du mini formulaire

--- projet-next/app/crud/page.tsx
+++ projet-next/app/crud/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const Crud = () => {
   return (
-    <div>Page de crud </div>
+    <div className="w-3/4 mx-auto mt-6 rounded-xl px-4 py-6 shadow-sm">
+      <div className="flex flex-col items-center space-y-8">
+        <h1 className="text-2xl font-bold">Remplissez vos taches </h1>
+        
+        <div className="w-full max-w-sm space-y-2">
+          <Label htmlFor="Titre" className='font-semibold'>Titre</Label>
+          <Input type="text" id="Titre" placeholder="Titre" />
+
+          <Label htmlFor="Message" className='font-semibold'>Message</Label>
+          <Input type="text" id="Message" placeholder="Message" />
+        </div>
+        
+        <Button className="mt-2 font-bold">
+          Valider
+        </Button>
+      </div>
+    </div>
   )
 }
 
