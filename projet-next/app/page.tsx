fddc8df6c976533012e40425d16758eb4a6477e7
YacineBoukliff <yacine69800@live.fr>
2024-12-18 12:06:34 +0100

    Test commit sur Vscode

--- projet-next/app/page.tsx
+++ projet-next/app/page.tsx
@@ -1,5 +1,4 @@
 import { DiAndroid } from "react-icons/di";
-import { DiApple } from "react-icons/di";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 
@@ -7,9 +6,8 @@ export default function Home() {
   return (
    <div>
     <DiAndroid /> 
-    <DiApple />
 
-
+    
     <Avatar>
   <AvatarImage src="https://github.com/shadcn.png" />
   <AvatarFallback>CN</AvatarFallback>
