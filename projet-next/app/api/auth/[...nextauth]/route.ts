5cde7033ada99c04478cda4e1ac7850c30c29cbf
YacineBoukliff <yacine69800@live.fr>
2024-12-22 10:54:47 +0100

    Setup Connexion avec google avec la BDD et fix de l'erreur

--- projet-next/app/api/auth/[...nextauth]/route.ts
+++ projet-next/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,24 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-const handler = NextAuth({
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-})
+  session: {
+    strategy: 'database'  
+  }
+};
 
 
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+
+
+export { handler as GET, handler as POST };
