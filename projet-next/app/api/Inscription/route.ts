837bc2746c56d1a55f7a63d72daaf5451c45e4f5
YacineBoukliff <yacine69800@live.fr>
2024-12-22 11:58:43 +0100

    Destructuration de l'api POST Inscription

--- projet-next/app/api/Inscription/route.ts
+++ projet-next/app/api/Inscription/route.ts
@@ -1,17 +1,29 @@
 import SchemaInscription from "@/app/schemas/SchemaInscription";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt"
 
 export async function POST(request : NextRequest){
-    const body = await request.json()
-    const validation = SchemaInscription.safeParse(body)
+    const {email , password} = await request.json()
+    const validation = SchemaInscription.safeParse({email , password})
 
-    if(validation.success) return NextResponse.json({ error : "Vous devez rentrez un mail et un mot de passe valide"}, {status : 404})
+    if(!validation.success) return NextResponse.json({ error : "Vous devez rentrez un mail et un mot de passe valide"}, {status : 404})
 
-    const email = await prisma.user.findUnique({
-        where : { email : body.email}
+    const UtilisateurExistant = await prisma.user.findUnique({
+        where : { email}
     })
     
-    if(email) return NextResponse.json({error : "Cet email existe déja "})
+    if(UtilisateurExistant) return NextResponse.json({error : "Cet email existe déja "}, {status: 400})
+
+  const HashedPassword = await bcrypt.hash(password, 10)
+
+  const NouvelUtilisateur = await prisma.user.create({
+    data : {
+        email,
+        HashedPassword
+    }
+  })
+
+  return NextResponse.json({email : NouvelUtilisateur.email})
     
 }
